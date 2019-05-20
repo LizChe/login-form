081cd0db1c72844de73d1e4164504dbae0110192
Liz <pachaffy@gmail.com>
2019-05-20 15:46:34 +0200

    Implement SessionDaoImpl.java

--- src/main/java/com/codecool/login/dao/SessionDaoImpl.java
+++ src/main/java/com/codecool/login/dao/SessionDaoImpl.java
@@ -1,15 +1,19 @@
 package com.codecool.login.dao;
 
-import com.codecool.login.model.Session;
+import java.util.ArrayList;
+import java.util.List;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class SessionDao implements Dao {
+import com.codecool.login.model.Session;
+
+public class SessionDaoImpl implements Dao {
 
     @Override
-    public int create(Session session) {
+    public int create(Session session) throws DaoException {
         String query = "INSERT INTO sessions "
                 + "(user_name, user_password, session_id) "
                 + "VALUES(?, ?, ?)";
@@ -25,7 +29,7 @@ public class SessionDao implements Dao {
     }
 
     @Override
-    public int update(Session session) {
+    public int update(Session session) throws DaoException {
         String query = "UPDATE sessions "
                 + "SET user_name = ?, user_password = ?, session_id = ? "
                 + "WHERE id = ?";
@@ -41,9 +45,56 @@ public class SessionDao implements Dao {
         }
     }
 
+    @Override
+    public List<Session> getSessions() throws DaoException {
+        List<Session> sessions;
+        String query = "SELECT id, user_name, user_password, session_id "
+                + "FROM sessions";
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            sessions = getSessionsFrom(preparedStatement);
+
+        } catch (SQLException e) {
+            throw  new DaoException("Failed to get sessions.\n" + e.getMessage());
+        }
+        return sessions;
+    }
+
     private void setRequiredStatements(PreparedStatement preparedStatement, Session session) throws  SQLException {
         preparedStatement.setString(1, session.getUserName());
         preparedStatement.setString(2, session.getUserPassword());
         preparedStatement.setString(3, session.getSessionId());
+    }
+
+    private List<Session> getSessionsFrom(PreparedStatement preparedStatement) throws DaoException {
+        List<Session> sessions = new ArrayList<>();
+        Session session;
+
+        String userName;
+        String userPassword;
+        String sessionId;
+        int id;
+
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            while(resultSet.next()) {
+                userName = resultSet.getString("user_name");
+                userPassword = resultSet.getString("user_password");
+                sessionId = resultSet.getString("session_id");
+                id = resultSet.getInt("id");
+
+                session = new Session.Builder()
+                        .withId(id)
+                        .withUserName(userName)
+                        .withUserPassword(userPassword)
+                        .withSessionId(sessionId)
+                        .build();
+
+                sessions.add(session);
+            }
+        } catch (SQLException e) {
+            throw new DaoException("Failed to populate the list of sessions.\n" + e.getMessage());
+        }
+        return sessions;
     }
 }
