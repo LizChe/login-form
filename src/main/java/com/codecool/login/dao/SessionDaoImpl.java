62a73eeff6a6afd6df4fb58943a45ca4795c5dbd
Liz <pachaffy@gmail.com>
2019-05-23 22:43:06 +0200

    Implement methods that handle salt from the database, add salt column in SessionDao methods

--- src/main/java/com/codecool/login/dao/SessionDaoImpl.java
+++ src/main/java/com/codecool/login/dao/SessionDaoImpl.java
@@ -15,8 +15,8 @@ public class SessionDaoImpl implements Dao {
     @Override
     public int create(Session session) throws DaoException {
         String query = "INSERT INTO sessions "
-                + "(user_name, user_password, session_id) "
-                + "VALUES(?, ?, ?)";
+                + "(user_name, user_password, session_id, salt) "
+                + "VALUES(?, ?, ?, ?)";
         try (Connection connection = DatabaseConnector.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
@@ -31,14 +31,14 @@ public class SessionDaoImpl implements Dao {
     @Override
     public int update(Session session) throws DaoException {
         String query = "UPDATE sessions "
-                + "SET user_name = ?, user_password = ?, session_id = ? "
+                + "SET user_name = ?, user_password = ?, session_id = ?, salt = ? "
                 + "WHERE user_name = ? AND user_password = ?";
         try (Connection connection = DatabaseConnector.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
             setRequiredStatements(preparedStatement, session);
-            preparedStatement.setString(4, session.getUserName());
-            preparedStatement.setString(5, session.getUserPassword());
+            preparedStatement.setString(5, session.getUserName());
+            preparedStatement.setString(6, session.getUserPassword());
 
             return preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -49,7 +49,7 @@ public class SessionDaoImpl implements Dao {
     @Override
     public List<Session> getSessions(String sessionId) throws DaoException {
         List<Session> sessions;
-        String query = "SELECT id, user_name, user_password, session_id "
+        String query = "SELECT id, user_name, user_password, session_id, salt "
                 + "FROM sessions WHERE session_id = ?";
         try (Connection connection = DatabaseConnector.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
@@ -66,7 +66,7 @@ public class SessionDaoImpl implements Dao {
     @Override
     public List<Session> getSessions(String userName, String userPassword) throws DaoException {
         List<Session> sessions;
-        String query = "SELECT id, user_name, user_password, session_id "
+        String query = "SELECT id, user_name, user_password, session_id, salt "
                 + "FROM sessions WHERE user_name = ? AND user_password = ?";
         try (Connection connection = DatabaseConnector.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
@@ -96,10 +96,26 @@ public class SessionDaoImpl implements Dao {
         }
     }
 
+    @Override
+    public byte[] getSaltBy(String userName) throws DaoException {
+        String query = "SELECT salt from sessions "
+                + "WHERE user_name = ?";
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, userName);
+            return getBytesFrom(preparedStatement);
+
+        } catch (SQLException e) {
+            throw new DaoException("Failed to get salt.\n" + e.getMessage());
+        }
+    }
+
     private void setRequiredStatements(PreparedStatement preparedStatement, Session session) throws SQLException {
         preparedStatement.setString(1, session.getUserName());
         preparedStatement.setString(2, session.getUserPassword());
         preparedStatement.setString(3, session.getSessionId());
+        preparedStatement.setBytes(4, session.getSalt());
     }
 
     private List<Session> getSessionsFrom(PreparedStatement preparedStatement) throws DaoException {
@@ -110,6 +126,7 @@ public class SessionDaoImpl implements Dao {
         String userPassword;
         String sessionId;
         int id;
+        byte[] salt;
 
         try (ResultSet resultSet = preparedStatement.executeQuery()) {
             while (resultSet.next()) {
@@ -117,12 +134,14 @@ public class SessionDaoImpl implements Dao {
                 userPassword = resultSet.getString("user_password");
                 sessionId = resultSet.getString("session_id");
                 id = resultSet.getInt("id");
+                salt = resultSet.getBytes("salt");
 
                 session = new Session.Builder()
                         .withId(id)
                         .withUserName(userName)
                         .withUserPassword(userPassword)
                         .withSessionId(sessionId)
+                        .withSalt(salt)
                         .build();
 
                 sessions.add(session);
@@ -131,5 +150,17 @@ public class SessionDaoImpl implements Dao {
             throw new DaoException("Failed to populate the list of sessions.\n" + e.getMessage());
         }
         return sessions;
+    }
+
+    private byte[] getBytesFrom(PreparedStatement preparedStatement) throws DaoException {
+        byte[] salt = null;
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            if (resultSet.next()) {
+                salt = resultSet.getBytes("salt");
+            }
+        } catch (SQLException e) {
+            throw new DaoException("Failed to get bytes from statement\n" + e.getMessage());
+        }
+        return salt;
     }
 }
